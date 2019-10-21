c05f09f4cd74af29d8aa7078efc81f0d528e124d
gs <gaoshan_yy@migu.cn>
2019-10-21 14:53:41 +0800

    修复ios打包脚本, 可以省去第四个参数

--- ios/build_ios_framework.sh
+++ ios/build_ios_framework.sh
@@ -12,33 +12,34 @@ fi
 
 PROJECT_FULL_PATH=$3
 
-TARGET_NAME=""
-if [ "$#" -eq 4 ]; then
-    TARGET_NAME=$4
-else
-    TARGET_NAME=$SDK_NAME
-fi
-
-
-# PROJECT_PATH=`echo $PROJECT_FULL_PATH | grep -o '^.*[/]'`
-# PROJECT_NAME=`echo $PROJECT_FULL_PATH | grep -o '[^/]*$' | cut -d '.' -f1`
 
 
 SDK_PATH=$HOME/Desktop/ios_frameworks/$SDK_NAME
 
+if [ -d "$SDK_PATH" ]; then
+  rm -rf $SDK_PATH
+fi
 
-rm -rf $SDK_PATH || true
-mkdir -p $SDK_PATH || true
+mkdir -p $SDK_PATH
 
 
 
-xcodebuild clean build -project $PROJECT_FULL_PATH -scheme $SDK_NAME -configuration $CONF -sdk iphonesimulator -arch x86_64 BUILD_DIR=${SDK_PATH}
-xcodebuild clean build -project $PROJECT_FULL_PATH -scheme $SDK_NAME -configuration $CONF -sdk iphoneos -arch armv7 -arch arm64 BUILD_DIR=${SDK_PATH}
+
+xcodebuild build -project $PROJECT_FULL_PATH -scheme $SDK_NAME -configuration $CONF -sdk iphonesimulator -arch x86_64 BUILD_DIR=${SDK_PATH}
+xcodebuild build -project $PROJECT_FULL_PATH -scheme $SDK_NAME -configuration $CONF -sdk iphoneos -arch armv7 -arch arm64 BUILD_DIR=${SDK_PATH}
 
 
 
 ARM_SDK_PATH=""
 X86_SDK_PATH=""
+
+
+if [ -d "${SDK_PATH}/${CONF}-iphoneos/${SDK_NAME}" ]; then
+  cd "${SDK_PATH}/${CONF}-iphoneos/${SDK_NAME}"
+  TARGET_NAME=`basename $(find . -name \*.framework -type d -maxdepth 1 -print | head -n1) | cut -d "." -f1`
+  cd -
+fi
+
 
 if [ -d "${SDK_PATH}/${CONF}-iphoneos/${SDK_NAME}/${TARGET_NAME}.framework" ]; then
    ARM_SDK_PATH=${SDK_PATH}/${CONF}-iphoneos/${SDK_NAME}/${TARGET_NAME}.framework
@@ -61,14 +62,17 @@ fi
 # name=`echo *.framework | cut -d "." -f1`
 
 # plutil -insert CFBundleSupportedPlatforms.1 -string iPhoneOS $X86_SDK_PATH/Info.plist
-cp -R ${ARM_SDK_PATH}/Modules/${TARGET_NAME}.swiftmodule/* $X86_SDK_PATH/Modules/${TARGET_NAME}.swiftmodule/ || true
+
+if [ -d "${ARM_SDK_PATH}/Modules/${TARGET_NAME}.swiftmodule" ]; then
+   cp -R ${ARM_SDK_PATH}/Modules/${TARGET_NAME}.swiftmodule/* $X86_SDK_PATH/Modules/${TARGET_NAME}.swiftmodule/
+fi
 
 lipo -create $ARM_SDK_PATH/$TARGET_NAME \
              $X86_SDK_PATH/$TARGET_NAME \
              -output $SDK_PATH/$TARGET_NAME
 
 cp -f $SDK_PATH/$TARGET_NAME $X86_SDK_PATH/$TARGET_NAME
-rm -rf $SDK_PATH/$TARGET_NAME || true
+rm -rf $SDK_PATH/$TARGET_NAME
 
 mkdir -p $SDK_PATH/universal
 cp -R ${X86_SDK_PATH} $SDK_PATH/universal/
