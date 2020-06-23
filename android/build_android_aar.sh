f9f8a791734ca44a3f7eddb2b1dc528fb49e3595
gs <gaoshan_yy@migu.cn>
2020-06-23 19:02:48 +0800

    fix android aar build

--- android/build_android_aar.sh
+++ android/build_android_aar.sh
@@ -12,6 +12,15 @@ fi
 
 PROJECT_FULL_PATH=$3
 
+MODULE_FULL_PATH=""
+if [ -z "$4" ]
+  then
+    MODULE_FULL_PATH=$3
+else
+	MODULE_FULL_PATH=$4
+fi
+
+
 
 SDK_PATH=$HOME/Desktop/android_aars/$SDK_NAME
 
@@ -22,10 +31,10 @@ mkdir -p $SDK_PATH/aar/
 
 
 cd $PROJECT_FULL_PATH
-rm -rf /$SDK_NAME/build/ || true
+rm -rf $MODULE_FULL_PATH/$SDK_NAME/build/ || true
 chmod +x gradlew
 ./gradlew :$SDK_NAME:clean && ./gradlew :$SDK_NAME:assemble${CONF}
 
-cp -f $SDK_NAME/build/outputs/aar/*.aar $SDK_PATH/aar/
+cp -f $MODULE_FULL_PATH/$SDK_NAME/build/outputs/aar/*.aar $SDK_PATH/aar/
 
 
