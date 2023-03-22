ef0d5eef945c9673614be9bbc06ae83ff83804df
gs <gaoshan_yy@migu.cn>
2023-03-22 16:01:25 +0800

    fix convert_quicktype.sh

--- csharp/convert_quicktype.sh
+++ csharp/convert_quicktype.sh
@@ -2,7 +2,7 @@
 
 
 vi \
--c ":%s/Ignore)]\n\( *\)public \(\w\+\)\([^?]\) /Ignore)]\r\1public \2\3? /g"  \
+-c ":%s/Ignore)]\n\( *\)public \([^ ]\+\)\([^?]\) /Ignore)]\r\1public \2\3? /g"  \
 -c ":%s/DisallowNull/Default/g" \
 -c ":%s/namespace /#nullable enable\r#pragma warning disable 8618\rnamespace /g" \
 -c ":%s/public static \(.\+\) FromJson(string json) \+=> \+JsonConvert.DeserializeObject<\(.*\)>(json, \(\w\+.\+\).Converter.Settings);/public static \1? FromJson(string json)\r        {\r            try { return JsonConvert.DeserializeObject<\1>(json, \3.Converter.Settings); } catch (System.Exception e) {\r#if UNITY_EDITOR || UNITY_STANDALONE ||UNITY_IOS || UNITY_ANDROID || UNITY_WSA || UNITY_WEBGL\r                UnityEngine.Debug.LogError(e);\r\r                UnityEngine.Debug.LogError(\"error json str: \" + json);\r#else\r                System.Console.Error.WriteLine(e);\r                System.Console.Error.WriteLine(\"error json str: \" + json);\r#endif\r                return null;\r            }\r        }/g" \
